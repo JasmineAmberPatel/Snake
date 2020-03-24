7a8392f4b8f8684a5c8a9bfb8dce7d8d50abf0ab
Jasmine Amber <jasmineamber.ja@gmail.com>
2020-03-24 12:55:34 +0000

    setup complete

---
+++ snake.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("snake");
+const context = canvas.getContext("2d");
+
+const box = 32;
+
+const ground = new Image();
+ground.src = "images/background.png";
+const food = new Image();
+foodImg.src = "images/pizza.png";
