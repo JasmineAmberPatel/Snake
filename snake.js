e1cfef7567f860a6b35093c4048f997a0953dff1
Jasmine Amber <jasmineamber.ja@gmail.com>
2020-03-25 09:48:44 +0000

    fixed background

--- snake.js
+++ snake.js
@@ -4,7 +4,7 @@ const context = canvas.getContext("2d");
 const box = 32;
 
 const ground = new Image();
-ground.src = "images/background.png";
+ground.src = "images/ground.png";
 
 const foodImg = new Image();
 foodImg.src = "images/food.png";
